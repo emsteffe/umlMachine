462740b62a26ef29c7bf07f5e63e2faf0e113f46
Erik Steffens <emsteffe@live.com>
2013-04-24 10:16:50 -0500

    forgot with last commit

--- src/org/umlMachine/view/UMLMachineView.java
+++ src/org/umlMachine/view/UMLMachineView.java
@@ -180,6 +180,7 @@ public class UMLMachineView extends AbstractView {
            		String line = reader.readLine();
            		HashSet<TransitionData> data = new HashSet<TransitionData>();
            		//transition is now multiline able.
+           		//
            		while(line != null){
            			if(line.contains("<transition ")){
            				TransitionData toAdd = new TransitionData();
